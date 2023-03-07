f26fc9dccf057931c47e22ca6d9813141c6ea45f
netti <welshnn@outlook.com>
2023-03-06 20:13:09 -0500

    wishlist app recycler view

---
+++ Projects/Wishlist/app/src/main/java/com/example/wishlist/WishItem.kt
@@ -0,0 +1,7 @@
+package com.example.wishlist
+
+class WishItem (
+    val name:String,
+    val price:String,
+    val url:String
+    )
---
+++ Projects/Wishlist/app/src/main/java/com/example/wishlist/WishlistAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.wishlist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
+
+class WishlistAdapter (private val items: List<WishItem>):RecyclerView.Adapter<WishlistAdapter.ViewHolder>(){
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val nameTextView:TextView
+        val priceTextView:TextView
+        val urlTextView:TextView
+
+        init {
+            nameTextView = itemView.findViewById(R.id.nameTv)
+            priceTextView = itemView.findViewById(R.id.priceTv)
+            urlTextView = itemView.findViewById(R.id.urlTv)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val wishListView = inflater.inflate(R.layout.wish_item,parent, false)
+        return ViewHolder(wishListView)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val wish = items.get(position)
+        holder.nameTextView.text = wish.name
+        holder.priceTextView.text = wish.price
+        holder.urlTextView.text = wish.url
+    }
+}
